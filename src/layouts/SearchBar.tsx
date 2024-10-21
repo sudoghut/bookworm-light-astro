ceda9c51ee8393fec930e8b8721ac405743a30c1
Hongsu Wang <sudospace@gmail.com>
2024-10-21 08:01:14 -0400

    Update search func placeholder

--- src/layouts/SearchBar.tsx
+++ src/layouts/SearchBar.tsx
@@ -70,7 +70,7 @@ export default function SearchBar({ searchList }: Props) {
     <div className="min-h-[45vh]">
       <input
         className="form-input w-full text-center"
-        placeholder="Type here to Search posts"
+        placeholder="Type Here to Search"
         type="text"
         name="search"
         value={inputVal}
